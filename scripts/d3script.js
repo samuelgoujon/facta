6321c5d151e5c5e62d0ed6285b2de9384a3934bf
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2018-08-26 19:27:28 +0400

    updates

--- scripts/d3script.js
+++ scripts/d3script.js
@@ -35,184 +35,45 @@ function renderChart(params) {
       calc.chartTopMargin = attrs.marginTop;
       calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
       calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
-      let expand = {};
-      let net;
-      let line = d3.line().x(d => d[0]).y(d => d[1]).curve(d3.curveCatmullRomClosed)
+
+      let padding = 15, clusterPadding = 100;
+      let line = d3.line().curve(d3.curveCatmullRomClosed)
       let color = d3.scaleOrdinal(d3.schemeCategory10)
 
       let zoom = d3.zoom()
           .scaleExtent([0.1, 10])
           .on("zoom", zoomed)
-        
-      var simulation = d3.forceSimulation()
-          .force("link", d3.forceLink().distance((l, i) => {
-            var n1 = l.source, n2 = l.target;
-            // larger distance for bigger groups:
-            // both between single nodes and _other_ groups (where size of own node group still counts),
-            // and between two group nodes.
-            //
-            // reduce distance for groups with very few outer links,
-            // again both in expanded and grouped form, i.e. between individual nodes of a group and
-            // nodes of another group or other group node or between two group nodes.
-            //
-            // The latter was done to keep the single-link groups ('blue', rose, ...) close.
-            return 50 +
-              Math.min(20 * Math.min((n1.size || (n1.group != n2.group ? n1.group_data.size : 0)),
-                                     (n2.size || (n1.group != n2.group ? n2.group_data.size : 0))),
-                   -30 +
-                   30 * Math.min((n1.link_count || (n1.group != n2.group ? n1.group_data.link_count : 0)),
-                                 (n2.link_count || (n1.group != n2.group ? n2.group_data.link_count : 0))),
-                   100);
-            //return 10;
-          }))
-          .force("charge", d3.forceManyBody())
-          .force("center", d3.forceCenter()
-                             .x(calc.chartWidth / 2)
-                             .y(calc.chartHeight / 2))
-          .force("x", d3.forceX(calc.chartWidth / 2))
-          .force("y", d3.forceY(calc.chartHeight / 2))
-          .force('collision', d3.forceCollide().radius(25).strength(1).iterations(60))
+      
+      let clusters = {};
+      attrs.data.nodes.forEach(d => {
+          const i = Math.floor(Math.random() * clusters.length)
+          d.radius = d.type == 'people' ? attrs.circleRadiusPeople : attrs.circleRadiusOrganizaion
+          d.x = Math.cos(i / clusters.length * 2 * Math.PI) * 600 + calc.chartWidth / 2 + Math.random(),
+          d.y = Math.sin(i / clusters.length * 2 * Math.PI) * 600 + calc.chartHeight / 2 + Math.random()
+          d.clusters = getGroups(d)
 
-      // constructs the network to visualize
-      function network(data, prev, expand) {
-        expand = expand || {};
-        var gm = {},    // group map
-            nm = {},    // node map
-            lm = {},    // link map
-            gn = {},    // previous group nodes
-            gc = {},    // previous group centroids
-            nodes = [], // output nodes
-            links = []; // output links
-
-        // process previous nodes for reuse or centroid calculation
-        if (prev) {
-          prev.nodes.forEach(function(n) {
-            let groups = getGroups(n);
-
-            groups.forEach(i => {
-              var o;
-              if (n.size > 0) {
-                gn[i] = n;
-                n.size = 0;
-              } else {
-                o = gc[i] || (gc[i] = {x:0,y:0,count:0});
-                o.x += n.x;
-                o.y += n.y;
-                o.count += 1;
-              }
-            })
-            
-          });
-        }
-
-        // determine nodes
-        for (var k=0; k < data.nodes.length; ++k) {
-          var n = data.nodes[k],
-              groups = getGroups(n)
-          // if (groups.length > 1) debugger
-          groups.forEach(i => {
-            l = gm[i] || (gm[i] = gn[i]) || (gm[i]={ group:i, size: 0, nodes:[]});
-            if (expand[i]) {
-              // the node should be directly visible
-              nm[n.node] = nodes.length;
-              if (nodes.indexOf(n) == -1) {
-                nodes.push(n);
-              }
-              if (gn[i]) {
-                // place new nodes at cluster location (plus jitter)
-                n.x = gn[i].x + Math.random();
-                n.y = gn[i].y + Math.random();
-              }
-              n.isGroup = false;
-            } else {
-              // the node is part of a collapsed cluster
-              if (l.size == 0) {
-                // if new cluster, add to set and position at centroid of leaf nodes
-                nm[i] = nodes.length;
-                l.isGroup = true;
-                nodes.push(l);
-                if (gc[i]) {
-                  l.x = gc[i].x / gc[i].count;
-                  l.y = gc[i].y / gc[i].count;
-                }
-              }
-              l.nodes.push(n);
-              //if (nodes.filter(x => x.group == "Présidence, Premier Ministre").length) debugger
-            }
-
-            // always count group size as we also use it to tweak the force graph strengths/distances
-            l.size += 1;
-            n.group_data = l;
+          d.clusters.forEach(cluster => {
+            if (!clusters[cluster] || (d.radius > clusters[cluster].radius)) clusters[cluster] = d;
           })
-              
-        }
-
-        for (i in gm) { gm[i].link_count = 0; }
-
-        // determine links
-        for (k=0; k < data.links.length; ++k) {
-          var e = data.links[k],
-              uG = getGroups(e.source),
-              vG = getGroups(e.target);
-          
-          //if (uG.length > 1 || vG.length > 1) debugger
-
-          uG.forEach(u => {
-            vG.forEach(v => {
-              if (u != v) {
-                gm[u].link_count++;
-                gm[v].link_count++;
-              }
-              u = expand[u] ? nm[e.source.node] : nm[u];
-              v = expand[v] ? nm[e.target.node] : nm[v];
-              var i = (u < v ? u + "|" + v : v + "|" + u),
-                  l = lm[i] || (lm[i] = {source:u, target:v, size:0});
-              l.size += 1;
-            })
-          })
-        }
-        for (i in lm) { links.push(lm[i]); }
-
-        return { nodes: nodes, links: links };
-      }
-
+        })
+      
       function getGroups(d) {
-        return d.group.split(',').map(d => d.trim())
+        if (!d.group.length) {
+          return []
+        }
+        return d.group.split(',').map(x => x.trim())
       }
 
-      function convexHulls(nodes, offset) {
-        var hulls = {};
-
-        // create point sets
-        for (var k=0; k < nodes.length; ++k) {
-          var n = nodes[k];
-          if (n.size) continue;
-          if (n.group.split(',').length == 1) {
-            let i = n.group;
-            let l = hulls[i] || (hulls[i] = []);
-
-            l.push([n.x-offset, n.y-offset]);
-            l.push([n.x-offset, n.y+offset]);
-            l.push([n.x+offset, n.y-offset]);
-            l.push([n.x+offset, n.y+offset]);
-          }
-        }
-
-        // create convex hulls
-        var hullset = [];
-        for (i in hulls) {
-          hullset.push({group: i, path: d3.polygonHull(hulls[i])});
-        }
-
-        return hullset;
-      }
-
-      attrs.data.links.forEach(d => {
-        let source = d.source;
-        let target = d.target;
-        d.source = attrs.data.nodes.filter(d => d.node == source)[0]
-        d.target = attrs.data.nodes.filter(d => d.node == target)[0]
-      })
+      var simulation = d3.forceSimulation()
+          .alpha(0.3)
+          .force("center", d3.forceCenter().x(calc.chartWidth / 2).y(calc.chartHeight / 2))
+          .force('collide', d3.forceCollide(d => d.radius + padding))
+          .nodes(attrs.data.nodes)
+          .on("tick", ticked)
+      
+      simulation.force("link", d3.forceLink().id(d => d.node).links(attrs.data.links))
+      
+      simulation.restart();
 
       //Drawing containers
       var container = d3.select(this);
@@ -233,74 +94,54 @@ function renderChart(params) {
       var linksGroup = chart.patternify({ tag: 'g', selector: 'links' })
       var nodesGroup = chart.patternify({ tag: 'g', selector: 'nodes' })
 
-      function init() {
-        net = network(attrs.data, net, expand);
-        simulation
-          .nodes(net.nodes)
-          .on("tick", ticked);
-  
-        simulation.force("link")
-          .links(net.links);
+      var link = linksGroup.patternify({ tag: 'line', selector: 'link', data: attrs.data.links })
+        .attr("stroke-width", d => 1)
+        .attr("stroke", '#ccc')
+        .attr("stroke-dasharray", d => d.type == 'dotted' ? 2 : 0);
+      
+      var node = nodesGroup.patternify({ tag: 'g', selector: 'node', data: attrs.data.nodes })
+          .attr('data-group', d => d.group)
+          .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
 
-        simulation.restart();
-        
-        var hull = hullsGroup.patternify({ tag: 'path', selector: 'hull', data: convexHulls(net.nodes, 15) })
-            .attr("d", d => {
-              return line(d.path)
-            })
-            .attr('data-group', d => d.group)
-            .attr("fill", d => color(d.group))
-            .on("click", function(d) {
-              expand[d.group] = false; 
-              init();
-            })
-            .call(d3.drag()
-              .on('start', group_dragstarted)
-              .on('drag', group_dragged)
-              .on('end', group_dragended)
-              );
-
-        var link = linksGroup.patternify({ tag: 'line', selector: 'link', data: net.links })
-          .attr("stroke-width", d => d.size || 1)
-          .attr("stroke", '#ccc')
-          .attr("stroke-dasharray", d => d.type == 'dotted' ? 2 : 0);
-        
-        var node = nodesGroup.patternify({ tag: 'g', selector: 'node', data: net.nodes })
-            .attr('data-group', d => d.group)
-            .call(d3.drag()
-                  .on("start", dragstarted)
-                  .on("drag", dragged)
-                  .on("end", dragended));
-
-        node.patternify({ tag: 'circle', selector: 'node-circle', data: d => [d] })
-          .attr("r", d => {
-            return d.type == "people" ? attrs.circleRadiusPeople : attrs.circleRadiusOrganizaion
-          })
-          .attr('data-name', d => d.node)
-          .attr("stroke-width", 1.5)
-          .attr("stroke", 'black')
-          .attr('class', d => `node-circle node-${d.type}`)
-          .on('click', function(d) {
-            let that = d3.select(this)
-            if (!d.isGroup) {
-              let r = (d.type == "people") ? attrs.circleRadiusPeople : attrs.circleRadiusOrganizaion
-              if (d.clicked) {
-                that.attr('r', r)
-                d.clicked = false
-                attrs.closeNav(d)
-              } else {
-                that.attr('r', r + 10)
-                d.clicked = true
-                attrs.openNav(d)
-              }
+      node.patternify({ tag: 'circle', selector: 'node-circle', data: d => [d] })
+        .attr("r", d => d.radius)
+        .attr('data-name', d => d.node)
+        .attr("stroke-width", 1.5)
+        .attr("stroke", 'black')
+        .attr('class', d => `node-circle node-${d.type}`)
+        .on('click', function(d) {
+          let that = d3.select(this)
+          if (!d.isGroup) {
+            let r = d.radius;
+            if (d.clicked) {
+              // that.attr('r', r)
+              d.clicked = false
+              attrs.closeNav(d)
+            } else {
+              // that.attr('r', r + 10)
+              d.clicked = true
+              attrs.openNav(d)
             }
-          })
-          .on('dblclick', function(d) {
-            expand[d.group] = !expand[d.group];
-            init();
-          })
-          .attr('fill', d => color(d.group))
-
+          }
+        })
+        .attr('fill', d => color(d.group))
+        
+      var hull = hullsGroup.patternify({ tag: 'path', selector: 'hull', data: Object.keys(clusters).map(c => {
+          return {
+            cluster: c,
+            nodes: node.filter(d => d.clusters.indexOf(c) > -1)
+          };
+        }) })
+      .attr("d", d => {
+        return line(d3.polygonHull(hullPoints(d.nodes)))
+      })
+      .attr('data-group', d => d.group)
+      .attr("fill", d => color(d.cluster))
+      .attr('opacity', 0.4);
+        
         // node.patternify({ tag: 'text', selector: 'node-text', data: d => [d] })
         //   .attr('text-anchor', 'middle')
         //   .attr('dy', d => d.type == "people" ? attrs.circleRadiusPeople + 30 : attrs.circleRadiusOrganizaion + 30)
@@ -311,20 +152,36 @@ function renderChart(params) {
         //     .call(wrap, attrs.circleRadiusOrganizaion * 2)
 
         function ticked() {
-          if (!hull.empty()) {
-            hull.data(convexHulls(net.nodes, 15))
-                .attr("d", d => line(d.path));
-          }
+          hull
+            .attr('d', d => line(d3.polygonHull(hullPoints(d.nodes))));
+
+          node
+            .each(cluster(0.1))
+            .each(collide(0.2))
+            .attr('transform', d => `translate(${d.x}, ${d.y})`)
 
           link
-              .attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
-  
-          node.attr('transform', d => `translate(${d.x}, ${d.y})`)
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
         }
-  
+        
+        function hullPoints(data) {
+          let pointArr = [];
+          const padding = 5;
+          data.each(d => {
+            const pad = d.radius + padding;
+            pointArr = pointArr.concat([
+              [d.x - pad, d.y - pad],
+              [d.x - pad, d.y + pad],
+              [d.x + pad, d.y - pad],
+              [d.x + pad, d.y + pad]
+            ]);
+          });
+          return pointArr;
+        }
+
         function dragstarted(d) {
           if (!d3.event.active) simulation.alphaTarget(0.3).restart();
           d.fx = d.x;
@@ -362,6 +219,60 @@ function renderChart(params) {
           d3.select(this).select('path').style('stroke-width', 1);
         }
 
+        function collide(alpha) {
+
+          const quadtree = d3.quadtree()
+            .x(function (d) { return d.x; })
+            .y(function (d) { return d.y; })
+            .extent([[0, 0], [calc.chartWidth, calc.chartHeight]])
+            .addAll(attrs.data.nodes);
+
+          return function (d) {
+            let r = d.radius + (attrs.circleRadiusOrganizaion * 8) + Math.max(padding, clusterPadding),
+                nx1 = d.x - r,
+                nx2 = d.x + r,
+                ny1 = d.y - r,
+                ny2 = d.y + r;
+            quadtree.visit(function (quad, x1, y1, x2, y2) {
+              let data = quad.data;
+              if (data && data !== d) {
+                let x = d.x - data.x,
+                    y = d.y - data.y,
+                    l = Math.sqrt(x * x + y * y),
+                    r = d.radius + data.radius + (d.cluster == data.cluster ? padding : clusterPadding);
+                if (l < r) {
+                  l = (l - r) / l * alpha;
+                  d.x -= x *= l;
+                  d.y -= y *= l;
+                  data.x += x;
+                  data.y += y;
+                }
+              }
+              return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            });
+          };
+        }
+
+        function cluster(alpha) {
+          // https://bl.ocks.org/mbostock/7881887
+          return function (d) {
+            if (!d.clusters.length || !d.clusters.length > 1) return;
+            const cluster = clusters[d.clusters[0]];
+            if (cluster === d) return;
+            let x = d.x - cluster.x,
+                y = d.y - cluster.y,
+                l = Math.sqrt(x * x + y * y),
+                r = d.radius + cluster.radius + 3;
+            if (l != r) {
+              l = (l - r) / l * alpha;
+              d.x -= x *= l;
+              d.y -= y *= l;
+              cluster.x += x;
+              cluster.y += y;
+            }
+          };
+        }
+
         function wrap(text, width) {
           text.each(function() {
             var text = d3.select(this),
@@ -385,15 +296,11 @@ function renderChart(params) {
             }
           });
         }
-      }
 
       //Zoom functions
       function zoomed() {
         chart.attr("transform", d3.event.transform)
       }
-
-      // initilize the chart
-      init();
 
       // Smoothly handle data updating
       updateData = function () {
